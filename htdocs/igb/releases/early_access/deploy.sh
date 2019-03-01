73c0d994bff1ef8a9366fd05e75041973a94a2a7
aloraine <aloraine@uncc.edu>
2019-03-01 20:49:09 +0000

    Do not hard-code master branch in deploy script

--- htdocs/igb/releases/early_access/deploy.sh
+++ htdocs/igb/releases/early_access/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # run this to deploy IGB early access (master branch)
 release="master"
-installers="IGB-$release.dmg IGB-$release.sh IGB-$release.exe IGB-$release-x64.exe IGB-master.sh"
+installers="IGB-$release.dmg IGB-$release.sh IGB-$release.exe IGB-$release-x64.exe IGB-$release.sh"
 base="https://bitbucket.org/lorainelab/integrated-genome-browser/downloads"
 for installer in $installers;
 do
