9b68725117ae5810cc100e41c9f303fe692c3cd1
Philip Badzuh <pbadzuh@uncc.edu>
2021-03-01 11:22:14 -0500

    IGBF-2801: Load top menu bar before the rest of the page content

--- htdocs/js/modalVideo.js
+++ htdocs/js/modalVideo.js
@@ -1,14 +1,8 @@
 // Render menu bar
 $(window).on("load", () => {
 
-    var html5Video = document.getElementById("htmlVideo");
-
-    // Load walkthrough video after the rest of the page loads.
-    // - Allows menu bar to load without waiting for the slow mp4 request.
-    html5Video.querySelector('source').src ='https://www.dropbox.com/s/isx033lv0cwmftl/bioVizConnectIntro.mp4?raw=1';
-    html5Video.innerHTML = html5Video.innerHTML;
-
     $('#html5Video').on('hidden.bs.modal', function() {
+        var html5Video = document.getElementById("htmlVideo");
         if (html5Video != null) {
             html5Video.pause();
         }
