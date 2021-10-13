c4e0206bee1accc384a7a17fc01ab1c3b4505d1b
Philip Badzuh <pbadzuh@uncc.edu>
2021-10-13 09:27:05 -0400

    IGBF-2972: Refactor hub facade api naming

--- htdocs/js/trackhub/util.js
+++ htdocs/js/trackhub/util.js
@@ -2,8 +2,8 @@ const BACKEND_DOMAIN = "http://127.0.0.1:8000";
 
 export function convertURL(url) {
   return BACKEND_DOMAIN.includes("http")
-    ? `${BACKEND_DOMAIN}/rest_api/?hubUrl=${url}&fileName=/`
-    : `https://${BACKEND_DOMAIN}/rest_api/?hubUrl=${url}&fileName=/`;
+    ? `${BACKEND_DOMAIN}/api/?hubUrl=${url}&fileName=/`
+    : `https://${BACKEND_DOMAIN}/api/?hubUrl=${url}&fileName=/`;
 }
 
 // Close tooltips on outside click
