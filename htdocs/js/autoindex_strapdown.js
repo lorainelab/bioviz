d3ddf7b581149b65f97587fff9f587f48bb5f756
DESKTOP-9TTSBJ2\Karthik Raveendran <kraveend@uncc.edu>
2022-11-22 13:54:40 -0500

    IGBF-3173 Using Github-jsDelivr link to strapdown.verbose.js

--- htdocs/js/autoindex_strapdown.js
+++ htdocs/js/autoindex_strapdown.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 	// Create a script element to fetch Strapdown.js.
 	strapdown = document.createElement('script');
-	strapdown.setAttribute('src', conf.strapdown ? conf.strapdown : 'https://bitbucket.org/lorainelab/strapdown.js/raw/main/strapdown.verbose.js');
+	strapdown.setAttribute('src', conf.strapdown ? conf.strapdown : 'https://cdn.jsdelivr.net/gh/lorainelab/strapdown.js@main/strapdown.verbose.js');
 
 	// Create an XMP element for Strapdown.js to process.
 	xmp = document.createElement('xmp'),
